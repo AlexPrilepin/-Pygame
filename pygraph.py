0ba6f5c98fd2079a2c059fdbed69c59e5ee8467e
AlexPrilepin <111271681+AlexPrilepin@users.noreply.github.com>
2022-12-15 23:00:57 +0200

    Add files via upload

--- pygraph.py
+++ pygraph.py
@@ -27,8 +27,13 @@ npc_bullet = pygame.image.load('npc_bullet.png')
 npc_bullet.set_colorkey((0, 0, 0))
 courpse = pygame.image.load('courpse_1.png')
 courpse.set_colorkey((255, 255, 255))
+hp_bar = pygame.image.load('health_points.png')
+hp_bar.set_colorkey((255, 255, 255))
+hc = pygame.image.load('hero_dead.png')
+hc.set_colorkey((255, 255, 255))
 
 aim = True
+GG = False
 courpses = []
 
 class Nepice:
@@ -39,7 +44,7 @@ class Nepice:
         self.x = x
         self.hp = 10
         self.damage = 1
-        self.cd = 90
+        self.cd = 50
 
     def get_damage(self, amount):
         global courpses
@@ -49,17 +54,18 @@ class Nepice:
         
             
     
-    def move(self, hp):
-        self.x += self.nx * v / fps
-        self.y += self.ny * v / fps
-        if self.x <= 30:
-            self.nx *= -1
-        if self.y <= 50:
-            self.ny *= -1
-        if self.x > width - 100:
-            self.nx *= -1
-        if self.y > height - 120:
-            self.ny *= -1
+    def move(self, hp, stop):
+        if not stop:
+            self.x += self.nx * v / fps
+            self.y += self.ny * v / fps
+            if self.x <= 30:
+                self.nx *= -1
+            if self.y <= 50:
+                self.ny *= -1
+            if self.x > 1200 - 100:
+                self.nx *= -1
+            if self.y > 800 - 120:
+                self.ny *= -1
         self.draw(hp)
         
     def draw(self):
@@ -138,7 +144,10 @@ class Hero_bullet:
         self.end_y = end_y
         self.damage = 4
         self.x, self.y = self.start_x, self.start_y
-        self.angle = int(asin((self.start_y - self.end_y) / sqrt((self.end_x - self.start_x) ** 2 + (self.end_y - self.start_y) ** 2)) / pi * 180)
+        try:
+            self.angle = int(asin((self.start_y - self.end_y) / sqrt((self.end_x - self.start_x) ** 2 + (self.end_y - self.start_y) ** 2)) / pi * 180)
+        except ZeroDivisionError:
+            self.angle = int(asin((self.start_y - self.end_y) / 0.00000000000000001) / pi * 180)
 
     def draw(self):
         if self.start_x < self.end_x:
@@ -174,18 +183,24 @@ class Hero:
         self.target_y = 600
         self.draw()
         self.angle = 0
+        self.hp = 10
+        self.total = 10
+        self.cd = 0
 
     def get_damage(self, num):
-        pass
+        global GG
+        self.hp -= num
+        if self.hp <= 0:
+            GG = True
 
     def move(self, m_x, m_y):
         if self.x + m_x <= 10:
             return
         if self.y + m_y <= 40:
             return
-        if self.x + m_x > width - 100:
+        if self.x + m_x > 1100 - 100:
             return
-        if self.y + m_y > height - 125:
+        if self.y + m_y > 800 - 125:
             return
         self.x += m_x
         self.y += m_y
@@ -201,11 +216,13 @@ class Hero:
     
     def shoot(self, s_x, s_y):
         global bullets
-        if aim:
-            bull = Hero_bullet(self.x, self.y, self.target_x, self.target_y)
-        else:
-            bull = Hero_bullet(self.x, self.y, s_x, s_y)
-        bullets.append(bull)
+        if self.cd == 0:
+            if aim:
+                bull = Hero_bullet(self.x, self.y, self.target_x, self.target_y)
+            else:
+                bull = Hero_bullet(self.x, self.y, s_x, s_y)
+            bullets.append(bull)
+            self.cd = 10
         
     def draw(self):
         if aim:
@@ -225,49 +242,53 @@ class Hero:
                 g_l_r = hero_l.get_rect(
                     topleft=(self.x, self.y))
                 screen.blit(hero_l, g_l_r)
-                g_l_r = self.curr_weapon[1].get_rect(
-                    topleft=(self.x + 22, self.y + 42))
-                rot_image = pygame.transform.rotate(self.curr_weapon[1], 0)
-                rot_rect = rot_image.get_rect(center=g_l_r.center)
-                rot_image.set_colorkey((24, 29, 35))
-                screen.blit(rot_image, rot_rect)
+                if not GG:
+                    g_l_r = self.curr_weapon[1].get_rect(
+                        topleft=(self.x + 22, self.y + 42))
+                    rot_image = pygame.transform.rotate(self.curr_weapon[1], 0)
+                    rot_rect = rot_image.get_rect(center=g_l_r.center)
+                    rot_image.set_colorkey((24, 29, 35))
+                    screen.blit(rot_image, rot_rect)
             else:
                 g_l_r = hero_r.get_rect(
                     topleft=(self.x, self.y))
                 screen.blit(hero_r, g_l_r)
-                g_l_r = self.curr_weapon[0].get_rect(
-                    topleft=(self.x + 22, self.y + 42))
-                rot_image = pygame.transform.rotate(self.curr_weapon[0], 0)
-                rot_rect = rot_image.get_rect(center=g_l_r.center)
-                rot_image.set_colorkey((24, 29, 35))
-                screen.blit(rot_image, rot_rect)
+                if not GG:
+                    g_l_r = self.curr_weapon[0].get_rect(
+                        topleft=(self.x + 22, self.y + 42))
+                    rot_image = pygame.transform.rotate(self.curr_weapon[0], 0)
+                    rot_rect = rot_image.get_rect(center=g_l_r.center)
+                    rot_image.set_colorkey((24, 29, 35))
+                    screen.blit(rot_image, rot_rect)
         else:
             if self.target_x < self.x:
                 g_l_r = hero_l.get_rect(
                     topleft=(self.x, self.y))
                 screen.blit(hero_l, g_l_r)
-                g_l_r = self.curr_weapon[1].get_rect(
-                    topleft=(self.x + 22, self.y + 42))
-                rot_image = pygame.transform.rotate(self.curr_weapon[1], int(1 * self.angle))
-                rot_rect = rot_image.get_rect(center=g_l_r.center)
-                rot_image.set_colorkey((24, 29, 35))
-                screen.blit(rot_image, rot_rect)
+                if not GG:
+                    g_l_r = self.curr_weapon[1].get_rect(
+                        topleft=(self.x + 22, self.y + 42))
+                    rot_image = pygame.transform.rotate(self.curr_weapon[1], int(1 * self.angle))
+                    rot_rect = rot_image.get_rect(center=g_l_r.center)
+                    rot_image.set_colorkey((24, 29, 35))
+                    screen.blit(rot_image, rot_rect)
             else:
                 g_l_r = hero_r.get_rect(
                     topleft=(self.x, self.y))
                 screen.blit(hero_r, g_l_r)
-                g_l_r = self.curr_weapon[0].get_rect(
-                    topleft=(self.x + 22, self.y + 42))
-                rot_image = pygame.transform.rotate(self.curr_weapon[0], int(-1 * self.angle))
-                rot_rect = rot_image.get_rect(center=g_l_r.center)
-                rot_image.set_colorkey((24, 29, 35))
-                screen.blit(rot_image, rot_rect)
+                if not GG:
+                    g_l_r = self.curr_weapon[0].get_rect(
+                        topleft=(self.x + 22, self.y + 42))
+                    rot_image = pygame.transform.rotate(self.curr_weapon[0], int(-1 * self.angle))
+                    rot_rect = rot_image.get_rect(center=g_l_r.center)
+                    rot_image.set_colorkey((24, 29, 35))
+                    screen.blit(rot_image, rot_rect)
 
 if __name__ == '__main__':
     npc_shoot = 0
     pygame.init()
     pygame.display.set_caption('Движущийся круг 2')
-    size = width, height = 1200, 800
+    size = width, height = 1200, 900
     screen = pygame.display.set_mode(size)
     x_pos, y_pos = 0, 0
     running = True
@@ -281,8 +302,14 @@ if __name__ == '__main__':
     waves = 3
     freeze_waves = 0
     win = 0
+    GG = False
+    pygame.mixer.music.load('grobik.mp3')
+    ccc = 0
     while running:
-        npc_shoot += 1
+        if hero.cd > 0:
+            hero.cd -= 1
+        if not GG:
+            npc_shoot += 1
         screen.fill((0, 0, 0))
         for a in range(1):
             for b in range(1):
@@ -292,36 +319,37 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEMOTION:
-                if not aim:
+            if not GG:
+                if event.type == pygame.MOUSEMOTION:
+                    if not aim:
+                        s_x, s_y = event.pos
+                        hero.target(s_x - 50, s_y - 50)
+                if event.type == pygame.MOUSEBUTTONUP:
                     s_x, s_y = event.pos
-                    hero.target(s_x - 50, s_y - 50)
-            if event.type == pygame.MOUSEBUTTONUP:
-                s_x, s_y = event.pos
-                hero.shoot(s_x - 50, s_y - 50)
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_a] == True and keys[pygame.K_s] == True:
-                hero.move(-10, 10)
-            elif keys[pygame.K_d] == True and keys[pygame.K_s] == True:
-                hero.move(10, 10)
-            elif keys[pygame.K_a] == True and keys[pygame.K_w] == True:
-                hero.move(-10, -10)
-            elif keys[pygame.K_d] == True and keys[pygame.K_w] == True:
-                hero.move(10, -10)
-            elif keys[pygame.K_a] == True:
-                hero.move(-10, 0)
-            elif keys[pygame.K_d] == True:
-                hero.move(10, 0)
-            elif keys[pygame.K_s] == True:
-                hero.move(0, 10)
-            elif keys[pygame.K_w] == True:
-                hero.move(0, -10)
-            if keys[pygame.K_q] == True:
-                aim = not aim
+                    hero.shoot(s_x - 50, s_y - 50)
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_a] == True and keys[pygame.K_s] == True:
+                    hero.move(-10, 10)
+                elif keys[pygame.K_d] == True and keys[pygame.K_s] == True:
+                    hero.move(10, 10)
+                elif keys[pygame.K_a] == True and keys[pygame.K_w] == True:
+                    hero.move(-10, -10)
+                elif keys[pygame.K_d] == True and keys[pygame.K_w] == True:
+                    hero.move(10, -10)
+                elif keys[pygame.K_a] == True:
+                    hero.move(-10, 0)
+                elif keys[pygame.K_d] == True:
+                    hero.move(10, 0)
+                elif keys[pygame.K_s] == True:
+                    hero.move(0, 10)
+                elif keys[pygame.K_w] == True:
+                    hero.move(0, -10)
+                if keys[pygame.K_q] == True:
+                    aim = not aim
         if len(npc) == 0 and waves_count < waves:
             if freeze_waves >= 90:
                 npc = []
-                for i in range(choice(list(range(3, 10)))):
+                for i in range(choice(list(range(3 + waves_count * 2, 10 + waves_count)))):
                     x_pos, y_pos = randint(50, 1100), randint(50, 700)
                     ball = Goblin(x_pos, y_pos)
                     npc.append(ball)
@@ -340,32 +368,39 @@ if __name__ == '__main__':
         g_l_r = wall_hor.get_rect(
                 topleft=(0, 0))
         screen.blit(wall_hor, g_l_r)
-        g_l_r1 = wall_hor.get_rect(
-                topleft=(0, 747))
-        screen.blit(wall_hor, g_l_r1)
         g_l_r2 = wall_ver.get_rect(
                 topleft=(0, 3))
         screen.blit(wall_ver, g_l_r2)
         g_l_r3 = wall_ver.get_rect(
                 topleft=(1165, 3))
         screen.blit(wall_ver, g_l_r3)
+        g_l_r1 = wall_hor.get_rect(
+                topleft=(0, 747))
+        screen.blit(wall_hor, g_l_r1)
         for i in courpses:
             g_l_r2 = courpse.get_rect(
                 topleft=(i[0], i[1]))
             screen.blit(courpse, g_l_r2)
         hero.draw()
+        pygame.draw.rect(screen, (0, 0, 0), (0, 800, 1200, 100))
+        g_l_r1 = hp_bar.get_rect(
+                topleft=(400, 810))
+        screen.blit(hp_bar, g_l_r1)
+        pygame.draw.rect(screen, (255, 0, 0), (474, 830, int(230 * (hero.hp / hero.total)), 34))
         for num, x in enumerate(npc):
-            x.move(x.hp)
-            if x.hp <= 0:
-                del npc[num]
-            if npc_shoot % x.cd  == 0:
-                x.attack()
-        for num, i in enumerate(bullets):
-            if i.draw() is True:
-                del bullets[num]
-        for num, i in enumerate(npc_bullets):
-            if i.draw() is True:
-                del npc_bullets[num]
+            x.move(x.hp, GG)
+            if not GG:
+                if x.hp <= 0:
+                    del npc[num]
+                if npc_shoot % x.cd  == 0:
+                    x.attack()
+        if not GG:
+            for num, i in enumerate(bullets):
+                if i.draw() is True:
+                    del bullets[num]
+            for num, i in enumerate(npc_bullets):
+                if i.draw() is True:
+                    del npc_bullets[num]
         
         
         if waves_count == waves and len(npc) == 0 and win >= 0 and win < 60:
@@ -377,6 +412,14 @@ if __name__ == '__main__':
             n = pygame.font.Font(None, 120)
             t = n.render(f'Wave {waves_count + 1}: ', True, (255, 0, 0))
             screen.blit(t, (450, 400))
+        if GG:
+            hero_l, hero_r = hc, hc
+            n = pygame.font.Font(None, 240)
+            t = n.render(f'YOU LOSE !!!', True, (255, 50, 50))
+            screen.blit(t, (80, 300))
+            if ccc == 0:
+                pygame.mixer.music.play()
+                ccc += 1
         
         clock.tick(fps)
         pygame.display.flip()
